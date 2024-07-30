b176708aa4906d2ab7df4bb4be3322b35acbf002
Muhammad Zahid Iqbal <56752369+Muhammad-Zahid-Iqbal@users.noreply.github.com>
2024-07-30 08:54:54 +0500

    update

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,6 +6,7 @@ import Appbar from "../components/appbar/Appbar";
 const Layout = ({ children }) => {
   return (
     <Box>
+      ///
       <Appbar/>
       <Box sx={{marginTop:"65px"}}>
         {children}
